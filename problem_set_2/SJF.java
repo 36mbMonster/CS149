5b374fdc61fef3c3dafd7a6b631c2ebbb20dd666
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-02-22 23:45:15 -0800

    SJF finished?

--- problem_set_2/SJF.java
+++ problem_set_2/SJF.java
@@ -4,35 +4,74 @@ public class SJF
 {
 	Random rand;
 	int procCount;
+	ArrayList<SimuProc> procs;
 	
 	public SJF(int procCount)
 	{
-		ArrayList<SimuProc> procs = new ArrayList<SimuProc>();
+		procs = new ArrayList<SimuProc>();
 		rand = new Random(System.currentTimeMillis());
 		this.procCount = procCount;
 		
 		for(int i = 0; i < procCount; i++)
-			procs.add(new SimuProc());		
+			procs.add(new SimuProc(i));		
 
 	}
 	
 	private class SimuProc
 	{
-		float a_time;
+		float arrival_time;
 		float et_runtime;
+		int id;
 		
-		private SimuProc()
+		private SimuProc(int id)
 		{
 			
-			a_time = rand.nextFloat() * 201;
-			et_runtime = rand.nextFloat() * (11f - .1f) + .1f;
+			arrival_time = rand.nextFloat() * 200.000001f;
+			et_runtime = rand.nextFloat() * (10.000001f - .1f) + .1f;
 			
-			System.out.println(a_time+" "+et_runtime);
+			this.id = id;
 		}
+
+		@Override
+		public String toString(){return "\nPID="+id+" at:"+arrival_time+" etr: "+et_runtime;}
+		private float getETR(){return et_runtime;}
+		private float getAT(){return arrival_time;}
 	}
 
 	public void runSJF()
 	{
+		Collections.sort(procs, new Comparator<SimuProc>()
+		{
+			@Override
+			public int compare(SimuProc p1, SimuProc p2) {return Float.compare(p1.getETR(), p2.getETR());}
+		});
 
+		System.out.println(procs);
+
+		statistics();
+	}
+
+	public void statistics()
+	{
+		float avgTurnAround = 0;
+		float avgWait = 0;
+		float throughput;
+		float totalTime = 0;
+
+		for(int i = 0; i < procs.size(); i++)
+		{
+			avgWait += procs.get(i).getETR();
+			avgTurnAround += procs.get(i).getETR()*i+1;
+			totalTime += procs.get(i).getETR();
+		}
+		avgTurnAround /= procs.size();
+		avgWait /= procs.size();
+
+		throughput = procs.size() / totalTime;
+
+		System.out.println("\n\nAverage Turn Around: "+avgTurnAround);
+		System.out.println("Average Wait: "+avgWait);
+		System.out.println("Average Response: "+avgWait);
+		System.out.println("Throughput:"+throughput);
 	}
 }
