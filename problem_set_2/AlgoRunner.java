7f8683468586e6943f9936b3b11e26c6029f89d6
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-02-22 23:48:31 -0800

    Put SJF call in a loop so that it will run 5 times.

--- problem_set_2/AlgoRunner.java
+++ problem_set_2/AlgoRunner.java
@@ -2,18 +2,19 @@ public class AlgoRunner
 {
 	public static void main(String[]args)
 	{
-		int procCount = Integer.parseInt(args[1]);
 
-		if(args.length != 2 || (procCount != 50 && procCount != 100))
+		if(args.length != 2 || (Integer.parseInt(args[1]) != 50 && Integer.parseInt(args[1]) != 100))
 		{
 			System.out.println("Useage: java AlgoRunner [time slice] [50 | 100]");
 		}
 		else
 		{
 			//Run the Algorithms here.
-			SJF sjf = new SJF(procCount);
+			SJF sjf = new SJF(Integer.parseInt(args[1]));
 
-			sjf.runSJF();
+			//Run SJF 5 times.
+			for(int i = 0; i < 5; i++)
+				sjf.runSJF();
 		}
 	}
 }
