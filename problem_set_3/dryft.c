e796d2600f1b5211dff7ee939d0790d5f434838b
Jon Vaccaro <jonathan.vaccaro@sjsu.edu>
2015-03-08 20:55:05 -0700

    This isn't anywhere near done. Just commiting to so I can move to a different computer. Added dynamic arrays for drivers and riders.

--- problem_set_3/dryft.c
+++ problem_set_3/dryft.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int riders;
-int drivers;
+int num_riders;
+int num_drivers;
 int time_window;
+
+typedef struct
+{
+    int id;
+}rider;
+
+typedef struct
+{
+    int id;
+}driver;
+
+rider *riders;
+driver *drivers;
 
 int main(int argc, char **argv)
 {
-    if(argc < 3)
+    if(argc != 4)
     {
         printf("Useage: dryft <number of riders> <number of drivers> <time window>\n");
         return 0;
     }
 
-    char *N = argv[0];
-    char *M = argv[1];
-    char *T = argv[2];
+    num_riders = atoi(argv[0]);
+    num_drivers = atoi(argv[1]);
+    time_window = atoi(argv[2]);
 
-    riders = atoi(N);
-    drivers = atoi(M);
-    time_window = atoi(T);
+    //dynamically allocate arrays
+    riders = malloc(num_riders * sizeof(rider));
+
+    int i;
+    for(i = 0; i < num_riders; i++)
+        riders[i].id = i;
+
+    printf("%d\n",num_riders);
 }
